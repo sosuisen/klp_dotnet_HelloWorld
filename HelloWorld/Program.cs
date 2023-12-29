62622986cb74b10499dc1ea138b9c23f680b1fd1
sosuisen <hidekazu.kubota@gmail.com>
2023-12-29 20:43:27 +0900

    add practice2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
