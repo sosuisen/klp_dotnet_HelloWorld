be9a81d8eda0e617a244233b48d86d0f30f77e53
sosuisen <hidekazu.kubota@gmail.com>
2023-12-29 20:47:04 +0900

    add practice3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Enter the first number: ");
+string? arg1 = Console.ReadLine();
+double.TryParse(arg1, out double num1);
+
+Console.Write("Enter the second number: ");
+string? arg2 = Console.ReadLine();
+double.TryParse(arg2, out double num2);
+
+Console.Write("Enter an operator (+, -, *, /): ");
+string? op = Console.ReadLine();
+
+double result = 0;
+
+switch (op)
+{
+    case "+":
+        result = num1 + num2;
+        break;
+    default:
+        Console.WriteLine("Invalid operator.");
+        return;
+}
+Console.WriteLine("Result: " + result);
