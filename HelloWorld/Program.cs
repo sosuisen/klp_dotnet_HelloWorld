ed55775bb6e2d968f62e202409e642293c87b405
sosuisen <hidekazu.kubota@gmail.com>
2024-02-12 01:08:42 +0900

    feat: add answer

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string hometown = "奈良";
+Console.WriteLine($"I was born in {hometown}, which is a great city.");
